8a5d13b36a33bdb03564031d93889ba3a55d300a
abdulraheem777 <kakaabdulraheem@gmail.com>
2024-11-18 18:25:06 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  `mongodb+srv://abdul:abdul@books-store-mern.svz2j.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN`
+
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
